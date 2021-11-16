9a3921501f678905d8b417e2826fafa537af2a94
raykin <raykincoldxiao@gmail.com>
2021-11-16 20:24:12 +0800

    Update CameraTest page

--- src/views/CameraTest.vue
+++ src/views/CameraTest.vue
@@ -7,10 +7,16 @@ ion-page
       ion-title CameraTest
 
   ion-content
-    h2 Camera Test Page
+    h2.ion-margin Camera Test Page
     div.big-box.ion-margin
       p Indented fill up many spaces
       ion-button(@click="takeMultiImages") Select Multi Images
+      br
+      ion-button(@click="getPhoto") Take Photo
+      br
+      ion-button(@click="cropPhoto") Crop Photo
+      br
+      ion-button(@click="signInApple") Apple SignIn
 
     ion-input(placeholder="Type something")
 
@@ -19,7 +25,9 @@ ion-page
 <script>
 import { IonPage, IonHeader, IonContent, IonInput } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import { Camera } from '@capacitor/camera';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { Crop } from 'ionic-native';
+import { SignInWithApple } from '@capacitor-community/apple-sign-in';
 
 export default defineComponent({
   name: 'Camera',
@@ -38,6 +46,38 @@ export default defineComponent({
 				})
 				console.warn(photos)
 				return photos
+			},
+			async getPhoto() {
+				const img = await Camera.getPhoto({
+					quality: 90,
+					allowEditing: true,
+					resultType: CameraResultType.Uri
+				})
+				console.warn(img.webPath)
+				return img;
+			},
+			async cropPhoto() {
+				const img = await Camera.getPhoto({
+					quality: 90,
+					allowEditing: true,
+					resultType: CameraResultType.Uri
+				})
+				const opts = { quality: 100, targetWidth: 320, targetHeight: 180 }
+				const crop_img = await Crop.crop(img.path, opts)
+				console.warn(crop_img)
+				return crop_img;
+			},
+			signInApple() {
+				let opts = {
+					clientId: "com.goodmoves.pwa.applelogin",
+					scope: "name email",
+					redirectURI: 'local-testing',
+					state: "client:web",
+				}
+				SignInWithApple.authorize(opts)
+					.then(result => {
+						console.warn(result)
+					})
 			}
 		}
 	}
